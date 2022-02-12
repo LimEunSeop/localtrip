8eebb20a2fdd2f3d54e9475f7eecef8458fbb9e7
LimEunSeop <dmstjq12@naver.com>
2022-02-12 23:58:24 +0900

    Add: 상단 메뉴 스타일링 + 칼라시스템 도입
    - 칼라시스템 참고사이트: https://colors.eva.design/
    - Layout 컴포넌트에 있는 styled-component 를 이제 응용할거 있으면 따로 분리해서 사용하면 됨 (ex. buttonReset 같은것들은 따로 믹스인 모음에다 둘것)
    - 내일할 일: 회원가입, 로그인 화면 만들기, 메뉴 반응형 적용 및 로그인버튼 로그아웃 으로 변경 기능 추가, Recoil 라이브러리로 JWT 로그인 처리 소스 추가

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
+import styled, { css } from 'styled-components'
 import { pages } from '../App'
+import logo from '../assets/images/logo.svg'
+import Color from '../assets/colors'
 
 interface Props {
   title: string
@@ -16,38 +19,128 @@ const Layout = ({ title, children }: Props) => {
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <header
-        style={{
-          textAlign: 'right',
-          padding: '20px',
-          fontSize: '24px',
-          fontWeight: 700,
-        }}
-      >
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">메인으로</Link>
-            </li>
-            {pages
-              .filter((page) => page.path !== '/login')
-              .map((page) => (
-                <li key={page.path}>
-                  <Link to={page.path}>{page.label}</Link>
-                </li>
-              ))}
-            <li>
-              <Link to="/login">로그인</Link>
-            </li>
-          </ul>
-        </nav>
+      <header>
+        <StyledNav>
+          <LogoLink to="/">
+            <img src={logo} alt="코나트립 로고" width="130" />
+          </LogoLink>
+
+          {pages
+            .filter((page) => page.path !== '/login')
+            .map((page) => (
+              <StyledNavLink to={page.path}>{page.label}</StyledNavLink>
+            ))}
+
+          <LoginLinkWrapper>
+            <LoginLink to="/login">로그인</LoginLink>
+          </LoginLinkWrapper>
+        </StyledNav>
       </header>
 
-      <main>{children}</main>
+      <main
+        style={{ height: '300px', textAlign: 'center', lineHeight: '300px' }}
+      >
+        {children}
+      </main>
 
-      <footer>Made By 코나아이 공채 12기 웹 개발그룹 임은섭.</footer>
+      <StyledFooter>
+        Made By 코나아이 공채 12기 웹 개발그룹 임은섭.
+      </StyledFooter>
     </>
   )
 }
+
+const StyledNav = styled.nav`
+  position: relative;
+  display: flex;
+`
+
+const LogoLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  margin-right: 30px;
+`
+
+const LoginLinkWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+`
+
+const StyledNavLink = styled(NavLink)`
+  height: 73px;
+  line-height: 73px;
+  font-size: 16px;
+  font-weight: 100;
+  padding: 0 23px;
+  border-top: 5px solid transparent;
+  color: #20303c;
+  text-decoration: none;
+  transition: 0.3s;
+
+  &.active {
+    border-top-color: ${Color.info600};
+  }
+
+  &:hover {
+    border-top-color: ${Color.info500};
+  }
+`
+const resetButton = css`
+  border: none;
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
+
+  background: transparent;
+
+  /* inherit font & color from ancestor */
+  color: inherit;
+  font: inherit;
+
+  /* Normalize 'line-height'. Cannot be changed from 'normal' in Firefox 4+. */
+  line-height: normal;
+
+  /* Corrects font smoothing for webkit */
+  -webkit-font-smoothing: inherit;
+  -moz-osx-font-smoothing: inherit;
+
+  /* Corrects inability to style clickable 'input' types in iOS */
+  -webkit-appearance: none;
+`
+
+const Button = styled.button`
+  ${resetButton}
+  border-radius: 9999px;
+`
+const LoginLink = styled(Link)`
+  ${resetButton}
+  display:block;
+  width: 94px;
+  height: 40px;
+  border-radius: 9999px;
+  border: 1px solid ${Color.info600};
+  text-align: center;
+  line-height: 40px;
+  text-decoration: none;
+  font-weight: 100;
+  color: ${Color.info600};
+  transition: 0.3s;
+
+  &:hover {
+    background: ${Color.info600};
+    color: #fff;
+  }
+`
+
+const StyledFooter = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+`
 
 export default Layout
